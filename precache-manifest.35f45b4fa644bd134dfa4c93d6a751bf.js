f269df2be2eceb1dcfd44184ba8b707077539b1d
Lerik <leriklerik@protonmail.com>
2021-10-07 11:13:18 -0300

    Updates

---
+++ precache-manifest.35f45b4fa644bd134dfa4c93d6a751bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb5685fbce7911a15571e5f39196bf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae2223d7c1e291f8a246",
+    "url": "/static/css/2.99131379.chunk.css"
+  },
+  {
+    "revision": "478366f6c4e20c5a04b6",
+    "url": "/static/css/main.7e7fc048.chunk.css"
+  },
+  {
+    "revision": "ae2223d7c1e291f8a246",
+    "url": "/static/js/2.3515109f.chunk.js"
+  },
+  {
+    "revision": "863460a7f8da7f527b1af3d94e6f8377",
+    "url": "/static/js/2.3515109f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478366f6c4e20c5a04b6",
+    "url": "/static/js/main.6b1b5a53.chunk.js"
+  },
+  {
+    "revision": "67ce43778e81ef2ec5d1",
+    "url": "/static/js/runtime-main.1b0170fe.js"
+  },
+  {
+    "revision": "0e51ca0ea0d3cdadd3f965aef453f6ab",
+    "url": "/static/media/Bathroom1.0e51ca0e.jpg"
+  },
+  {
+    "revision": "3a5d2cabc1626c88b47d4418c4e08659",
+    "url": "/static/media/Bathroom2.3a5d2cab.jpg"
+  },
+  {
+    "revision": "b0215de92bd552d17670d753e67806dc",
+    "url": "/static/media/Kitchen1.b0215de9.jpg"
+  },
+  {
+    "revision": "57264e25017749acf69f2eeebc3a3f90",
+    "url": "/static/media/Kitchen2.57264e25.jpg"
+  }
+]);
