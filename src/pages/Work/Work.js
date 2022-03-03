229530a7d4d1f4ba14aaab0167c76a7af30700f4
Valeriu Bahov <60433187+valeriubahov@users.noreply.github.com>
2022-03-03 08:53:34 -0400

    Update Work.js

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -9,7 +9,7 @@ const Work = ({ user }) => {
       <WorkItem>
         <WhiteBlock>
           <Paragraph>We proudly service the greater area of the HRM.</Paragraph>
-          <P1>CALL <a href="tel:+9028091815" class="headerContactItem">902-809-1815</a></P1>
+          <P1>CALL <a href="tel:+xxx-xxx-xxxx" class="headerContactItem">xxx-xxx-xxxx</a></P1>
           {/* <P1>EMAIL <a href="mailto:namesurname@gmail.com">namesurname@gmail.com</a></P1> */}
         </WhiteBlock>
       </WorkItem>
